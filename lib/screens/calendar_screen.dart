9260301ac1aeb3cd4cb0dde70761574cba8c68d6
unknown <ahmad.rafi02@ui.ac.id>
2021-08-24 19:54:35 +0700

    making calendar page

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,30 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
   static const routeName = '/calendar';
+
+  const CalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  final today = DateTime.now();
+  get firstDay => DateTime(today.year, today.month - 6, today.day);
+  get lastDay => DateTime(today.year, today.month + 6, today.day);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calendar Page"),
+        title: const Text("Calendar Page"),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
+            TableCalendar(
+              firstDay: firstDay,
+              lastDay: lastDay,
+              focusedDay: _focusedDay,
+              calendarFormat: _calendarFormat,
+              locale: 'en-US',
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                if (!isSameDay(_selectedDay, selectedDay)) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                }
+              },
+              onFormatChanged: (format) {
+                  if (_calendarFormat != format) {
+                    setState(() {
+                      _calendarFormat = format;
+                    });
+                  }
+                }
+            ),
             TextButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/timer');
                 },
                 child: const Text(
                   "Go TO TIMER",
-                  style: TextStyle(
-                    fontSize: 28
-                ),
-              )
-            ),
+                  style: TextStyle(fontSize: 20),
+                )),
           ],
         ),
       ),
