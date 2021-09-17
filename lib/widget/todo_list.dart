1dff6dc9ebf5d542bda51eec15113667659dac89
unknown <ahmad.rafi02@ui.ac.id>
2021-09-17 23:27:38 +0700

    change todolist widget

--- lib/widget/todo_list.dart
+++ lib/widget/todo_list.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: sized_box_for_whitespace
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -9,10 +7,7 @@ class ToDoList extends StatelessWidget {
   final List<Todo> todo;
   final Function deleteTodo;
 
-  ToDoList(
-    this.todo,
-    this.deleteTodo
-  );
+  const ToDoList(this.todo, this.deleteTodo);
 
   @override
   Widget build(BuildContext context) {
@@ -20,56 +15,10 @@ class ToDoList extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
         Container(
-          width: 370,
-          child: todo.isEmpty 
-            ? ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: const StadiumBorder(),
-                primary: const Color.fromRGBO(11, 128, 236, 1),
-                textStyle: const TextStyle(
-                  fontFamily: 'DMSans',
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400),
-              ),
-              onPressed: (){}, 
-              child: const Text(
-                'TODO',
-                )
-              )
-            : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card (
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text('\$${todo[index].date}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      todo[index].name!,
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(todo[index].date!),
-                    ),
-                    trailing: IconButton(
-                       icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTodo(todo[index].id),
-                    ),
-                  ),
-                );
-              }
-            ),
+          child: Text(
+            'TODO',
+          ),
+          color: const Color.fromRGBO(11, 128, 236, 1),
         ),
       ],
     );
