fe2858d29b03ffe71d6c10f9935c5698aa5631fa
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2021-09-18 21:21:36 +0700

    finishing calendar screen. NEXT: adding logic per point in calendar

--- lib/widget/todo_list.dart
+++ lib/widget/todo_list.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -32,8 +34,69 @@ class ToDoList extends StatelessWidget {
             ),
           ),
         ),
+        const SizedBox(
+          height: 20,
+        ),
         Container(
-          
+          height: 400,
+          child: todo.isEmpty
+              ? Column(
+                  children: <Widget>[
+                    const Text(
+                      'No TODO today. Maybe add one?',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'DMSans',
+                        fontSize: 22,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                        height: 100,
+                        child: Image.asset(
+                          'assets/images/waiting.png',
+                          fit: BoxFit.cover,
+                        )),
+                  ],
+                )
+              : ListView.builder(
+                  itemBuilder: (ctx, index) {
+                    return Card(
+                      elevation: 5,
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 5,
+                      ),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 30,
+                          child: Padding(
+                            padding: const EdgeInsets.all(6),
+                            child: FittedBox(
+                              child: Text('\$${todo[index].name}'),
+                            ),
+                          ),
+                        ),
+                        title: Text(
+                          todo[index].name!,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                        subtitle: Text(
+                          DateFormat.yMMMd().format(todo[index].date!),
+                        ),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTodo(todo[index].id),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: todo.length,
+                ),
         ),
       ],
     );
